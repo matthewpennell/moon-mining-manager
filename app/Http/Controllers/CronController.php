df707f722c91e9dc797d406ce74d380e678dfa65
Matthew Pennell <github@matthewpennell.com>
2017-10-14 15:40:52 +0100

    Untested cron process of moon mining logs.

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\EveController;
 use Illuminate\Http\Request;
 use App\Refinery;
+use App\MiningActivity;
+use App\Miner;
 
 class CronController extends EveController
 {
@@ -15,19 +17,13 @@ class CronController extends EveController
     public function pollRefineries()
     {
         
-        // Retrieve the prime user's character details.
-        $character = $this->esi->invoke('get', '/characters/{character_id}/', [
-            'character_id' => $this->user->eve_id,
-        ]);
-
         // Request a list of all of the active mining observers belonging to the corporation.
         $mining_observers = $this->esi->invoke('get', '/corporation/{corporation_id}/mining/observers/', [
-            'corporation_id' => $character->corporation_id,
+            'corporation_id' => $this->corporation_id,
         ]);
         
         // Process the refineries list. For each entry, we want to check and see if it already exists 
-        // in the database. If it does, we flag that it is currently active. If it doesn't, we create 
-        // a new database entry for it.
+        // in the database. If it doesn't, we create a new database entry for it.
         foreach ($mining_observers as $observer)
         {
             $refinery = Refinery::find($observer->observer_id);
@@ -43,19 +39,68 @@ class CronController extends EveController
                 $refinery->name = $structure->name;
                 $refinery->solar_system_id = $structure->solar_system_id;
             }
-            $refinery->is_active = 1;
             $refinery->save();
         }
 
     }
 
     /**
-     * For each active moon mining observer, we request details of the current mining activity log,
-     * parse it, and insert it into the database.
+     * For each moon mining observer, we request details of the current mining activity log,
+     * parse it, and insert it into the database. Any unrecognised miners are also added.
      */
     public function pollMiningObservers()
     {
         
+        // Grab all of the refineries and loop through them.
+        $refineries = Refinery::all();
+
+        foreach ($refineries as $refinery)
+        {
+            // Retrieve the mining activity log for this refinery.
+            $activity_log = $this->esi->invoke('get', '/corporation/{corporation_id}/mining/observers/{observer_id}/', [
+                'corporation_id' => $this->corporation_id,
+                'observer_id' => $refinery->observer_id,
+            ]);
+            foreach ($activity_log as $log_entry)
+            {
+                // Check whether this entry has already been recorded.
+                $existing_activity = MiningActivity::where([
+                    'miner_id' => $log_entry->character_id,
+                    'refinery_id' => $refinery->observer_id,
+                    'type_id' => $log_entry->type_id,
+                    'quantity' => $log_entry->quantity,
+                ])->get();
+                if ($existing_activity->isEmpty())
+                {
+                    // Create a new entry in the database for this activity.
+                    $mining_activity = new MiningActivity;
+                    $mining_activity->miner_id = $log_entry->character_id;
+                    $mining_activity->refinery_id = $refinery->observer_id;
+                    $mining_activity->type_id = $log_entry->type_id;
+                    $mining_activity->quantity = $log_entry->quantity;
+                    $mining_activity->save();
+                }
+                // Check if this miner is already known.
+                $existing_miner = Miner::where('eve_id', $log_entry->character_id)->get();
+                if ($existing_miner->isEmpty())
+                {
+                    // Create a new entry for this miner, including pulling additional information.
+                    $miner = new Miner;
+                    $miner->eve_id = $log_entry->character_id;
+                    $character = $this->esi->invoke('get', '/characters/{character_id}/', [
+                        'character_id' => $log_entry->character_id,
+                    ]);
+                    $miner->name = $character->name;
+                    $miner->corporation_id = $character->corporation_id;
+                    $portrait = $this->esi->invoke('get', '/characters/{character_id}/portrait/', [
+                        'character_id' => $log_entry->character_id,
+                    ]);
+                    $miner->avatar = $portrait->px128x128;
+                    $miner->save();
+                }
+            }
+        }
+
     }
 
 }
--- app/Http/Controllers/EveController.php
+++ app/Http/Controllers/EveController.php
@@ -9,11 +9,14 @@ use Seat\Eseye\Containers\EsiAuthentication;
 use Seat\Eseye\Eseye;
 use App\User;
 
+/**
+ * Parent controller for all other controllers making use of the EVE ESI API endpoints.
+ */
 class EveController extends Controller
 {
 
     protected $esi; // Eseye object for performing all ESI requests
-    protected $user; // reference to the prime user
+    protected $corporation_id; // reference to the prime user's corporation ID
 
     /**
      * Class constructor. Create an ESI API object to handle all requests.
@@ -26,14 +29,14 @@ class EveController extends Controller
         $configuration->datasource = env('ESEYE_DATASOURCE', 'tranquility');
 
         // Create authentication with app details and refresh token from user 1.
-        $this->user = User::first();
+        $user = User::first();
 
         // Need to request a new valid access token from EVE SSO using the refresh token of the original request.
         $url = 'https://sisilogin.testeveonline.com/oauth/token';
         $response = Curl::to($url)
             ->withData(array(
                 'grant_type' => "refresh_token",
-                'refresh_token' => $this->user->refresh_token
+                'refresh_token' => $user->refresh_token
             ))
             ->withHeaders(array(
                 'Authorization: Basic ' . base64_encode(env('TESTEVEONLINE_CLIENT_ID') . ':' . env('TESTEVEONLINE_CLIENT_SECRET'))
@@ -41,14 +44,14 @@ class EveController extends Controller
             ->enableDebug('logFile.txt')
             ->post();
         $new_token = json_decode($response);
-        $this->user->refresh_token = $new_token->refresh_token;
-        $this->user->save();
+        $user->refresh_token = $new_token->refresh_token;
+        $user->save();
 
         $authentication = new EsiAuthentication([
             'secret'        => env('TESTEVEONLINE_CLIENT_SECRET'),
             'client_id'     => env('TESTEVEONLINE_CLIENT_ID'),
             'access_token'  => $new_token->access_token,
-            'refresh_token' => $this->user->refresh_token,
+            'refresh_token' => $user->refresh_token,
             'scopes'        => [
                                 'esi-industry.read_corporation_mining.v1',
                                 'esi-wallet.read_corporation_wallet.v1',
@@ -60,6 +63,13 @@ class EveController extends Controller
 
         // Create ESI API object.
         $this->esi = new Eseye($authentication);
+
+        // Retrieve the prime user's character details.
+        $character = $this->esi->invoke('get', '/characters/{character_id}/', [
+            'character_id' => $user->eve_id,
+        ]);
+
+        $this->corporation_id = $character->corporation_id;
 
     }
     
