63f5ae89e4b8d5a4c0b0185d19360db91d525e70
Matthew Pennell <github@matthewpennell.com>
2017-10-22 11:20:41 +0100

    Switch Socialite to use live server instead of dev.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -18,11 +18,12 @@ class AuthController extends Controller
      */
     public function redirectToProvider()
     {
-        return Socialite::driver('eveonline-sisi')->scopes([
-            'esi-industry.read_corporation_mining.v1',
+        return Socialite::driver('eveonline')->scopes([
+//            'esi-industry.read_corporation_mining.v1',
             'esi-wallet.read_corporation_wallets.v1',
             'esi-mail.send_mail.v1',
             'esi-universe.read_structures.v1',
+            'esi-corporations.read_structures.v1',
         ])->redirect();
     }
 
@@ -35,7 +36,7 @@ class AuthController extends Controller
     {
         $esi = new EsiConnection;
         
-        $user = Socialite::driver('eveonline-sisi')->user();
+        $user = Socialite::driver('eveonline')->user();
         $authUser = $this->findOrCreateUser($user);
 
         // Check if the user is whitelisted to access the app.
