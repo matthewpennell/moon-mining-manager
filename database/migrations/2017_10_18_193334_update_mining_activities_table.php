08612da8c97a61c51b90fa19ee6b5b2f2e66e64c
Matthew Pennell <github@matthewpennell.com>
2017-10-25 13:21:12 +0100

    Fix class name clash in database migrations.

--- database/migrations/2017_10_18_193334_update_mining_activities_table.php
+++ database/migrations/2017_10_18_193334_update_mining_activities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateMiningActivitiesTable extends Migration
+class UpdateMiningActivitiesTable2 extends Migration
 {
     /**
      * Run the migrations.
