cc7339b80b8589b0b9cab4fa6cd4fa092964a46f
Matthew Pennell <github@matthewpennell.com>
2018-01-27 10:43:31 +0000

    Show end date of rental agreement in summary table.

--- resources/views/renters/home.blade.php
+++ resources/views/renters/home.blade.php
@@ -19,6 +19,7 @@
                         <th class="numeric">Monthly fee</th>
                         <th class="numeric">Currently owed</th>
                         <th class="numeric">Start date</th>
+                        <th class="numeric">End date</th>
                         <th>Edit</th>
                     </tr>
                 </thead>
@@ -37,6 +38,13 @@
                             <td class="numeric">{{ number_format($renter->monthly_rental_fee, 0) }} ISK</td>
                             <td class="numeric">{{ number_format($renter->amount_owed, 0) }} ISK</td>
                             <td class="numeric">{{ date('jS F Y', strtotime($renter->start_date)) }}</td>
+                            <td class="numeric">
+                                @if (isset($renter->end_date))
+                                    {{ date('jS F Y', strtotime($renter->end_date)) }}
+                                @else
+                                    -
+                                @endif
+                            </td>
                             <td><a href="/renters/{{ $renter->id }}">Edit details</td>
                         </tr>
                     @endforeach
